395a317f2a217704a4e73db433d449b40ba17a70
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-03-26 19:48:30 -0700

    Rendering post on comments view

--- KnockItOff/RedditPostViewController.swift
+++ KnockItOff/RedditPostViewController.swift
@@ -52,33 +52,83 @@ class RedditPostViewController: UIViewController {
 extension RedditPostViewController: RATreeViewDataSource{
     
     func treeView(treeView: RATreeView!, cellForItem item: AnyObject!) -> UITableViewCell! {
-        let cell = NSBundle.mainBundle().loadNibNamed("CommentTableViewCell", owner: self, options: nil)[0] as? CommentTableViewCell
-        if let comment = item as RKComment? {
-            cell?.comment = comment
+        if item.isKindOfClass(RKComment) {
+            let cell = NSBundle.mainBundle().loadNibNamed("CommentTableViewCell", owner: self, options: nil)[0] as? CommentTableViewCell
+            if let comment = item as RKComment? {
+                cell?.comment = comment
+            }
+            cell?.level = treeView.levelForCellForItem(item)
+            return cell
+            
+        } else if item.isKindOfClass(RKLink) {
+            let cell = NSBundle.mainBundle().loadNibNamed("PostDetailsTableViewCell", owner: self, options: nil)[0] as? PostDetailsTableViewCell
+            if let post = item as RKLink? {
+                cell?.post = post
+            }
+            return cell
+            
         }
-        cell?.level = treeView.levelForCellForItem(item)
-        return cell
+        
+        return UITableViewCell()
+//        let cell = NSBundle.mainBundle().loadNibNamed("CommentTableViewCell", owner: self, options: nil)[0] as? CommentTableViewCell
+//        if let comment = item as RKComment? {
+//            cell?.comment = comment
+//        }
+//        cell?.level = treeView.levelForCellForItem(item)
+//        return cell
     }
     
     func treeView(treeView: RATreeView!, numberOfChildrenOfItem item: AnyObject!) -> Int {
         if item == nil {
             if let c = comments? {
-                return c.count
+                return c.count + 1
             } else {
-                return 0
+                return 1
             }
         }
         
         let comment = item as RKComment
         return comment.replies.count
+//        if item == nil {
+//            if let c = comments? {
+//                return c.count + 0
+//            } else {
+//                return 0
+//            }
+//        }
+//        
+//        let comment = item as RKComment
+//        return comment.replies.count
+
     }
     
     func treeView(treeView: RATreeView!, child index: Int, ofItem item: AnyObject!) -> AnyObject! {
-        let comment = item as RKComment?
+        
+        
         if item == nil {
-            return comments![index]
+            if index == 0 {
+                return post
+            } else {
+                return comments![index-1]
+            }
+        } else {
+            let comment = item as RKComment?
+            return comment!.replies[index-1]
         }
-        return comment!.replies[index]
+        
+//        if let comment = item as RKComment? {
+//          return comment.replies[index]
+//        } else if let post = item as RKLink? {
+//            return post
+//        } else {
+//            return comments![index]
+//        }
+        
+//        let comment = item as RKComment?
+//        if item == nil {
+//            return comments![index]
+//        }
+//        return comment!.replies[index]
     }
 }
 
@@ -94,65 +144,4 @@ extension RedditPostViewController: RATreeViewDelegate {
         let cell = treeView.cellForItem(item) as CommentTableViewCell
         cell.expanded = false
     }
-//    func treeView(treeView: RATreeView!, indentationLevelForRowForItem item: AnyObject!) -> Int {
-//        
-//    }
-    
-//    func treeView(treeView: RATreeView!, didSelectRowForItem item: AnyObject!) {
-//        let comment = item as RKComment?
-//        println("comment: " + comment!.body)
-//        println("this comment has \(comment!.replies.count) replies")
-//        
-//        
-//        if treeView.isCellForItemExpanded(item) {
-//            treeView.collapseRowForItem(item, collapseChildren: true, withRowAnimation: RATreeViewRowAnimationAutomatic)
-//        } else {
-//            treeView.expandRowForItem(item, expandChildren: true, withRowAnimation: RATreeViewRowAnimationAutomatic)
-//        }
-//    }
-//    
-//    func treeView(treeView: RATreeView!, shouldExpandRowForItem item: AnyObject!) -> Bool {
-//        return true
-//    }
-//    
-//    func treeView(treeView: RATreeView!, shouldCollapaseRowForItem item: AnyObject!) -> Bool {
-//        return true
-//    }
 }
-
-//
-//extension RedditPostViewController: UITableViewDataSource, UITableViewDelegate {
-//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        return 2
-//    }
-//
-//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        if section == 0 {
-//            return 1
-//        } else if section == 1 {
-//            if let c = comments? {
-//                return c.count
-//            } else {
-//                return 0
-//            }
-//        }
-//        
-//        return 0
-//    }
-//    
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        switch indexPath.section {
-//        case RedditPostViewControllerTableViewSection.Post.rawValue:
-//            let cell = tableView.dequeueReusableCellWithIdentifier("PostDetailsTableViewCell") as PostDetailsTableViewCell
-//            cell.post = post
-//            return cell
-//        case RedditPostViewControllerTableViewSection.Comments.rawValue:
-//            let cell = tableView.dequeueReusableCellWithIdentifier("CommentTableViewCell") as CommentTableViewCell
-//            cell.comment = comments?[indexPath.row]
-//            return cell
-//        default:
-//            return UITableViewCell()
-//        }
-//    }
-//}
-
