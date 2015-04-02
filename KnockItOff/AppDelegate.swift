c957d6c8a9f2a14ed34085a6227a222ca8310ac3
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-04-01 21:18:10 -0700

    Tapping into app delegate for notifications status

--- KnockItOff/AppDelegate.swift
+++ KnockItOff/AppDelegate.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
+enum NotificationStatus: Int {
+    case NotPrompted = 0
+    case Authorized = 1
+    case Denied = 2
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var notificationStatus: NotificationStatus = .NotPrompted
     var window: UIWindow?
-
+    
+    private var notificationClosure:((NotificationStatus)->Void)! = nil
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
      
@@ -28,43 +36,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
-    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
-        println("Local notification received")
-    }
-    
     func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
         println("did register user notifications")
+        if notificationSettings.types == .None {
+            notificationStatus = .Denied
+        } else {
+            notificationStatus = .Authorized
+        }
+        if notificationClosure != nil {
+            notificationClosure(notificationStatus)
+        }
     }
-    
-    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
-        println("did register remote notifications")
-    }
-    
-    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
-        println("FAIL register remote notifications")
+
+    func promptForNotifcations(notificationClosure: (NotificationStatus) -> (Void)) {
+        UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Sound | UIUserNotificationType.Alert | UIUserNotificationType.Badge, categories: nil))
+        notificationStatus = .NotPrompted
+        self.notificationClosure = notificationClosure
     }
 }
 
--- KnockItOff/SettingsNotificationsViewController.swift
+++ KnockItOff/SettingsNotificationsViewController.swift
@@ -10,48 +10,60 @@ import UIKit
 
 class SettingsNotificationsViewController: UIViewController {
 
-    @IBOutlet weak var successLabel: UILabel!
-    @IBOutlet weak var nextButton: UIButton!
-    @IBOutlet weak var promptButton: UIButton!
+    @IBOutlet private weak var successLabel: UILabel!
+    @IBOutlet private weak var nextButton: UIButton!
+    @IBOutlet private weak var promptButton: UIButton!
+    @IBOutlet private weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
-        let status: UIUserNotificationSettings? = UIApplication.sharedApplication().currentUserNotificationSettings()
-        let types = status?.types
+//        let status: UIUserNotificationSettings? = UIApplication.sharedApplication().currentUserNotificationSettings()
+//        let types = status?.types
 
-//        if UIApplication.sharedApplication().enabledRemoteNotificationTypes() {
-//            println("already got notification permission")
-//            promptButton?.hidden = true
-//            nextButton?.hidden = false
-//            successLabel?.hidden = false
-//            
-//        } else {
-//            println("none. Display notifiation permissions")
-//            promptButton?.hidden = false
-//            nextButton?.hidden = true
-//            successLabel?.hidden = true
-//        }
-        if types == UIUserNotificationType.None {
-            println("none. Display notifiation permissions")
+
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let status: NotificationStatus = appDelegate.notificationStatus
+        
+        switch status {
+        case .NotPrompted:
+            println("Not Prompted")
             promptButton?.hidden = false
             nextButton?.hidden = true
             successLabel?.hidden = true
-        } else {
-            println("already got notification permission")
+            datePicker?.hidden = true
+
+        case .Authorized:
+            println("Authorized")
             promptButton?.hidden = true
             nextButton?.hidden = false
             successLabel?.hidden = false
+            datePicker?.hidden = false
+
+        case .Denied:
+            println("Denied")
+            promptButton?.hidden = true
+            nextButton?.hidden = false
+            successLabel?.hidden = false
+            datePicker?.hidden = true
         }
     }
-
+    
     @IBAction func promptButtonTouchUpInside(sender: AnyObject) {
-        let status: UIUserNotificationSettings? = UIApplication.sharedApplication().currentUserNotificationSettings()
-        UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Sound | UIUserNotificationType.Alert | UIUserNotificationType.Badge, categories: nil))
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        appDelegate.promptForNotifcations { (status) -> (Void) in
+            switch status {
+            case .NotPrompted:
+                println("Not Prompted")
+            case .Authorized:
+                println("Authorized")
+            case .Denied:
+                println("Denied")
+            }
+        }
         
-        successLabel?.hidden = false
-        promptButton?.hidden = true
-        nextButton?.hidden = false
+//        successLabel?.hidden = false
+//        promptButton?.hidden = true
+//        nextButton?.hidden = false
     }
 }
